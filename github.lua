0ce46a44a596ef4cc8964adf548471a96467deb4
Schmarni <marnistromer@gmail.com>
2022-01-26 03:03:44 +0100

    Create github.lua

---
+++ github.lua
@@ -0,0 +1,70 @@
+local tArgs = { ... }
+
+
+local function get(code,branch,user,repo)
+
+    if user == nil then
+        user = "Schmarni-Dev"
+    end
+    if repo == nil then
+        repo = "CCT_lua_stuff"
+    end
+    if branch == nil then
+        branch = "main"
+    end
+    local gitdata = user.."/"..repo.."/"..branch.."/".."/"..code
+
+
+    -- Add a cache buster so that spam protection is re-checked
+    local cacheBuster = ("%x"):format(math.random(0, 2 ^ 30))
+    local response, err = http.get(
+        "https://raw.githubusercontent.com/" .. textutils.urlEncode(gitdata) .. "?cb=" .. cacheBuster
+    )
+
+    if response then
+        -- If spam protection is activated, we get redirected to /paste with Content-Type: text/html
+        local headers = response.getResponseHeaders()
+        if not headers["Content-Type"] or not headers["Content-Type"]:find("^text/plain") then
+            io.stderr:write("Failed.\n")
+            print("IDK why")
+            return
+        end
+
+        print("Success.")
+
+        local sResponse = response.readAll()
+        response.close()
+        return sResponse
+    else
+        io.stderr:write("Failed.\n")
+        print(err)
+    end
+end
+
+if true then
+    -- Download a file from pastebin.com
+    if #tArgs < 2 then
+        return
+    end
+
+    -- Determine file to download
+    local sFile = tArgs[1]
+    local sCode = tArgs[2]
+    local sPath = shell.resolve(sFile)
+    if fs.exists(sPath) then
+        print("File already exists")
+        return
+    end
+
+    -- GET the contents from pastebin
+    local res = get(sCode,tArgs[3],tArgs[4],tArgs[5])
+    if res then
+        local file = fs.open(sPath, "w")
+        file.write(res)
+        file.close()
+
+        print("Downloaded as " .. sFile)
+    end
+end
+
+
